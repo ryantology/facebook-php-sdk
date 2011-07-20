b6e9572dcfb6b32b999d8a77ff0c0d44e88523eb
Ryan White <ryanw@craynenashe.com>
2011-07-20 12:47:16 -0400

    Added cakephp versions of abstract methods to store data using CakeSession

--- src/facebook_cakephp.php
+++ src/facebook_cakephp.php
@@ -6,7 +6,7 @@
 */
 
 try {
-	require_once "facebook.php";
+	require_once "base_facebook.php";
 } catch (Exception $execption) {
 	if ($execption->getMessage() != 'Facebook needs the CURL PHP extension.') {
 		throw $execption;
@@ -14,7 +14,7 @@ try {
 }
 
 
-class CakeFacebook extends Facebook {
+class CakeFacebook extends BaseFacebook {
 	
 	public function __construct($config) {
 		if (!session_id()) {
@@ -48,6 +48,53 @@ class CakeFacebook extends Facebook {
 		  throw $execption;
 		}
 		return $result;
+	}
+	
+	protected static $kSupportedKeys = array('state', 'code', 'access_token', 'user_id');
+
+  /**
+   * Provides the implementations of the inherited abstract
+   * methods.  The implementation uses PHP sessions to maintain
+   * a store for authorization codes, user ids, CSRF states, and
+   * access tokens.
+   */
+	protected function setPersistentData($key, $value) {
+		if (!in_array($key, self::$kSupportedKeys)) {
+			debug('Unsupported key passed to setPersistentData.');
+			return;
+		}
+		
+		$session_var_name = $this->constructSessionVariableName($key);
+		$this->Session->write($session_var_name, $value);
+	}
+
+	protected function getPersistentData($key, $default = false) {
+		if (!in_array($key, self::$kSupportedKeys)) {
+			debug('Unsupported key passed to getPersistentData.');
+			return $default;
+		}
+
+		$session_var_name = $this->constructSessionVariableName($key);
+		return isset($this->Session->read($session_var_name)) ?
+			$this->Session->read($session_var_name) : $default;
+	}
+
+	protected function clearPersistentData($key) {
+		if (!in_array($key, self::$kSupportedKeys)) {
+			debug('Unsupported key passed to clearPersistentData.');
+			return;
+		}
+
+		$session_var_name = $this->constructSessionVariableName($key);
+		$this->Session->delete($session_var_name);
+	}
+
+	protected function clearAllPersistentData() {
+		$this->Session->delete('fb.'.$this->getAppId());
+	}
+
+	protected function constructSessionVariableName($key) {
+		return implode('.', array('fb', $this->getAppId(), $key));
 	}
 }
 
